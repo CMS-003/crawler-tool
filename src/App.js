c50fbf80eeaa44422851cbf96fc0fc948372c9db
阮家友 <1439120442@qq.com>
2023-07-02 11:44:51 +0800

    fix await bug

--- src/App.js
+++ src/App.js
@@ -78,12 +78,10 @@ function App() {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url: window.location.href, extra: window.__extra || '', html: from === 'browser' ? document.documentElement.innerHTML : '' })
       });
-      console.log(resp, resp.status)
       if (resp.status !== 200) {
         setStatus(constant.S_FAIL);
       } else {
-        const body = resp.json();
-        console.log(body);
+        const body = await resp.json();
         if (body.status === -1) {
           setStatus(constant.S_FAIL);
         }
